15f18e37c068fcbf0c8ea4bb308ae41b60c9ef5b
tomasacuna <tomily748@gmail.com>
2022-09-21 21:36:39 -0300

    Primera entrega

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import logo from './logo.svg';
 //import './App.css';
 import './styles.css';
 
+
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
 import { Children } from 'react';
 
 function App() {
   return (
     <>
     <NavBar/>
-    <section className='todo'>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
-    <h1>Probando</h1>
+    <section className='bkground min-h-screen'>
+    <div className="tituloObjeto">
+
+      <ItemListContainer name='Chanchito' price='2600' />
+      <ItemListContainer name='Elefante' price='2500' />
+      <ItemListContainer name='Lobo' price='2800' />
+    </div>
+    
     </section>
     
+
   
     </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
+import CartWidget from './CartWidget'; <CartWidget/>
+
 const NavBar = () => {
   return (
     <header>
     <div className="navbar bg-base-100 navBarC">
         <div className="flex-1">
         <a className="navbar" href="index.html">
-                <img src="../img/TRlogo.png" class="logo" alt="logito"/>
+                <img src="../img/TRlogo.png" className="logo" alt="logito"/>
         </a>
             
         </div>
@@ -12,7 +14,8 @@ const NavBar = () => {
             <ul className="menu menu-horizontal p-0">
                 <li><a href="../../public/pages/productos.html">Productos</a></li> 
                 <li><a href="../../public/pages/contacto.html">Contacto</a></li>
-                <li><a href="../../public/pages/carrito.html">Carrito</a></li>
+                <li><a href="../../public/pages/carrito.html">Carrito <CartWidget/></a></li>
+                
         </ul>
         </div>
     </div>
